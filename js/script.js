85575b4757da1df745a36c9d7a4761c8401f4f66
unknown <trialsgama@gmail.com>
2018-10-12 12:08:53 -0300

    Proyecto terminado

--- js/script.js
+++
@@ -1,7 +0,0 @@
-function agregarCompartir() {
-    //var input = '<input type="hidden" name="nasa_id" value="' + nasa_id + '">'
-    var boton = '<button class="btncompartir" type="submit">Compartir</button>';
-    var form = '<form action="compartir.html" method="get">' + boton + '</form>';
-    $(form).appendTo('.resp');
-
-}
